799589c20d26fd2a5374b9f694322a9d445d9dba
nguyenquethi <nguyenquethi186@>
2023-02-16 18:32:28 -0500

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,8 @@
+/* Comp229-assignment1  Thi-Nguyen  301221756  04/02/2023*/
+
+(function(){
+    function Start(){
+        console.log("App started ...");
+    }
+    window.addEventListener("load", Start);
+})
