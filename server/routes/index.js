0773f10dd2c96cecfd556886bf1f91896702dbbf
nguyenquethi <nguyenquethi186@>
2023-02-20 00:10:36 -0500

    updated register

--- server/routes/index.js
+++ server/routes/index.js
@@ -27,4 +27,13 @@ router.get('/login', indexController.displayLoginPage);
 /* POST Route for processing the Login page */
 router.post('/login', indexController.processLoginPage);
 
+/* GET Route for displaying the Register page */
+router.get('/register', indexController.displayRegisterPage);
+
+/* POST Route for processing the Register page */
+router.post('/register', indexController.processRegisterPage);
+
+/* GET to perform UserLogout */
+router.get('/logout', indexController.performLogout);
+
 module.exports = router;
